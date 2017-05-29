3ac9f04feb023a2e32a1be3d19e80039bb326f26
viviana <vivianazamudio1908@gmail.com>
2017-05-29 16:33:39 -0500

    Proyecto final

---
+++ src/pages/add/add.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+@IonicPage()
+@Component({
+  selector: 'page-add',
+  templateUrl: 'add.html',
+})
+export class AddPage {
+  public todoItem: String;
+  public todoList: Array<String>
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  this.todoList =JSON.parse(localStorage.getItem("todos"));
+  if(!this.todoList){
+    this.todoList= [];
+    }
+   this.todoItem = ""; 
+  }
+
+  save(){
+    if(this.todoItem!=""){
+    this.todoList.push(this.todoItem);
+    localStorage.setItem("todos",JSON.stringify(this.todoList));
+    }
+  }
+ 
+}
